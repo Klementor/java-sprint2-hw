654f1113a3a243a324e291c61aabea4f2d8bb707
Крылов Михаил <arhangel373krylov@yandex.ru>
2022-04-20 08:59:29 +0300

    Fixes

--- src/MonthReport.java
+++ src/MonthReport.java
@@ -1,14 +1,10 @@
 import java.util.ArrayList;
 
 public class MonthReport {
-    private ArrayList<MonthTrade> trades = new ArrayList<>();
+    private final ArrayList<MonthTrade> trades = new ArrayList<>();
 
     public ArrayList<MonthTrade> getTrades() {
         return trades;
-    }
-
-    public void setTrades(ArrayList<MonthTrade> trades) {
-        this.trades = trades;
     }
 }
 
--- src/MonthTrade.java
+++ src/MonthTrade.java
@@ -5,40 +5,25 @@ class MonthTrade {
         this.quantity = quantity;
         this.sumOfOne = sumOfOne;
     }
-    private String itemName;
-    private Boolean isExpense;
-    private int quantity;
-    private int sumOfOne;
+    private final String itemName;
+    private final Boolean isExpense;
+    private final int quantity;
+    private final int sumOfOne;
 
     public int getQuantity() {
         return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
     }
 
     public int getSumOfOne() {
         return sumOfOne;
     }
 
-    public void setSumOfOne(int sumOfOne) {
-        this.sumOfOne = sumOfOne;
-    }
-
     public String getItemName() {
         return itemName;
-    }
-
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
     }
 
     public Boolean getExpense() {
         return isExpense;
     }
 
-    public void setExpense(Boolean expense) {
-        isExpense = expense;
-    }
 }
